b205b44ce810cba6ebf3fd76f63808f066e52bcb
Mahsa Rahmani Asl <mahrahmaniasl@gmail.com>
2025-03-20 13:37:55 +0100

    display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,27 @@
+import { HStack, Image, List, ListItem , Text} from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import getCroppedImageURL from "../services/image-url";
 
 const GenreList = () => {
   const { data, error } = useGenre();
 
   return (
     <>
-      {error && <p>{ error }</p>}
-      <ul>
+      {error && <p>{error}</p>}
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageURL(genre.image_background)}
+                borderRadius={8}
+                boxSize="32px"
+                />
+                <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
